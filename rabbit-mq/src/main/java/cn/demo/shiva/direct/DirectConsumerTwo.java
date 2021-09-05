9c9a1b4b53d9963317e10295fc0574c45dd00a9f
路的尽头在哪 <qianwei4712@163.com>
2021-09-05 20:28:16 +0800

    rabbitMQ

---
+++ rabbit-mq/src/main/java/cn/demo/shiva/direct/DirectConsumerTwo.java
@@ -0,0 +1,19 @@
+package cn.demo.shiva.direct;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * @author shiva   2021/9/4 21:46
+ */
+@Component
+@RabbitListener(queues = "directQueue")
+public class DirectConsumerTwo {
+    @RabbitHandler
+    public void process(Map<String, Object> message) {
+        System.out.println("消费者2，DirectReceiver 消费者收到消息  : " + message.toString());
+    }
+}
---
+++ rabbit-mq/src/main/java/cn/demo/shiva/direct/DirectProducer.java
@@ -0,0 +1,41 @@
+package cn.demo.shiva.direct;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.DateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * @author shiva   2021/9/4 21:27
+ */
+@RestController
+@RequestMapping
+public class DirectProducer {
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @GetMapping("/directMsg")
+    public String directMsg() {
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("messageId", String.valueOf(UUID.randomUUID()));
+        map.put("data", "发送数据体" + System.currentTimeMillis());
+        map.put("createTime", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+
+        //将消息携带绑定键值：directRouting 发送到交换机 directExchange
+        rabbitTemplate.convertAndSend("directExchange", "directRouting", map);
+
+        return JSONObject.toJSONString(map);
+    }
+
+}
