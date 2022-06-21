769cff0b3e79dd1cd7c629e3cde58181b3e0a9ae
qianwei4712@163.com <hqw3.14159263>
2022-06-21 23:51:49 +0800

    导出数据库文档小工具

---
+++ db-doc/src/main/java/com/example/doc/DbDocApplication.java
@@ -0,0 +1,13 @@
+package com.example.doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbDocApplication.class, args);
+    }
+
+}
